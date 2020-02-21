3bc78db76b344a21414f8435f070f341a0c51e95
LubaKaper <liubovkaper@pursuit.com>
2020-02-21 10:20:04 -0500

    Initial Commit

---
+++ Local-Notification-Lab/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Local-Notification-Lab
+//
+//  Created by Liubov Kaper  on 2/21/20.
+//  Copyright © 2020 Luba Kaper. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
